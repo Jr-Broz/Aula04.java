dbbeb50b8a36ecb8d118363e52ae7015bc5ff919
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-08-16 13:30:05 -0300

    Colocando no github arquivos do  exercicio de POO do gustavo guanabara

---
+++ Aula04.java
@@ -0,0 +1,48 @@
+class Aula04{
+
+    public static void main(String[] args) {
+
+        Caneta c1 = new Caneta();
+
+        c1.status();
+
+
+
+
+
+
+
+
+
+
+
+    //         //Metodo acessor
+    //     c1.setModelo(" Bic ");
+        
+    //     //Acessar diretamente pelo atributo. Não funciona caso acessibilidade = private
+    //    // c1.modelo = "Bic"; 
+
+
+
+    //     c1.setPonta( 0.5f);
+
+
+    //     //Nao pode ser acessado pois tem o modificador de acessibilidade privado
+    //     //c1.ponta = 0.9f;
+
+
+    // System.out.println("Possuo uma caneta" + c1.getModelo() + "de ponta " + c1.getPonta());
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
---
+++ Caneta.java
@@ -0,0 +1,74 @@
+public class Caneta {
+ 
+    
+private String modelo;
+private float ponta;
+private String cor = "Azul";
+private boolean tampada;
+
+public  Caneta(){ //Este é o método construtor
+
+
+this.tampar();
+this.cor = "Azul";
+this.modelo = "Bic";
+}
+
+
+public String getModelo(){
+
+    return this.modelo;
+}
+
+
+public void setModelo(String modelo){
+
+
+    this.modelo = modelo;
+
+}
+
+public float getPonta(){
+
+    return this.ponta;
+}
+
+public void setPonta(float ponta){
+
+    this.ponta = ponta;
+}
+
+
+public void tampar()  {
+
+    this.tampada = true;    
+}
+
+
+
+public void destampar(){
+
+    this.tampada = false;
+}
+
+
+
+
+
+public void status(){
+
+    System.out.println("Sobre a caneta: ");
+    System.out.println("Modelo: " + this.getModelo());
+    System.out.println("Ponta: " + this.getPonta());
+    System.out.println("Cor: " + this.cor);
+    System.out.println("Tampada: " + this.tampada);
+
+}
+
+
+
+
+
+
+
+}
